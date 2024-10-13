6e6a7bca07a136f18816df3328bd5ada902f6a65
Daniel-sql123 <dadelgadoal@est.utn.ac.cr>
2024-10-13 01:38:01 -0600

    Se crea clase Pieza
    Donde se crea una clase llamada Reina  con sus atributos y metodos

---
+++ Piezas/Reina.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Piezas;
+
+import Juego.CuadroPieza;
+import Juego.Tablero;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Reina extends Pieza {
+
+    public Reina(boolean blanca, int x, int y) {
+        super(blanca, x, y);
+    }
+
+    public Reina(int color) {
+        super(color);
+        setImagenPieza(new ImageIcon(getClass().getResource("/Imagenes/reina_" + (color == 1 ? "b" : "n"))));
+        setPeso(8);
+    }
+
+    public String toString() {
+        return "Reina" + (getColor() == 1 ? "blanco" : "negro");
+
+    }
+
+    @Override
+    public boolean puedeMoverse(CuadroPieza dest, Tablero tabl) {
+        Caballo C = new Caballo();
+        C.setCuadroPieza(getCuadroPieza());
+        if (C.ComprobarMovimientos(dest, tabl)) {
+            return super.ComprobarMovimientos(dest, tabl);
+        }
+        return false;
+    }
+}
+
