8b6a89dc14508eb564f9cb3c934ea4704f3428d2
Christian <crisp314528@gmail.com>
2024-10-13 01:28:05 -0600

    Clase Tablero y VistaCaballo
    Se crea la clase tablero con diseño de ajedrez la cual se refleja en el JDialog que se desprende el FrmMenu

--- Vista/FrmMenu.java
+++ Vista/FrmMenu.java
@@ -40,6 +40,11 @@ public class FrmMenu extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         Desk.setLayer(jButton1, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
@@ -73,6 +78,12 @@ public class FrmMenu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        ViewHorse view = new ViewHorse(this, true);
+        view.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
